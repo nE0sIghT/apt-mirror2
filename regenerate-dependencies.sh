b65cba81175af3eba3b0e4897e9819b923315ce4
Yuri Konotopov <ykonotopov@gnome.org>
2024-06-15 07:41:06 +0000

    pyproject: add pyinstaller to dev dependencies

---
+++ regenerate-dependencies.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+set -exo pipefail
+
+pip-compile --generate-hashes --output-file=requirements/prod.txt
+pip-compile --constraint=requirements/prod.txt --extra=uvloop --generate-hashes --output-file=requirements/uvloop.txt
+pip-compile --constraint=requirements/prod.txt --extra=dev --generate-hashes --output-file=requirements/dev.txt
+pip-compile --constraint=requirements/prod.txt --extra=aiofiles --generate-hashes --output-file=requirements/aiofiles.txt
+pip-compile --constraint=requirements/prod.txt --extra=prometheus --generate-hashes --output-file=requirements/prometheus.txt

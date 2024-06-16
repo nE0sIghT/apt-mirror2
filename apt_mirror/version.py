ec603f8663858cd246bbe51fdfc28d061eb52c16
Yuri Konotopov <ykonotopov@gnome.org>
2024-06-16 07:11:55 +0000

    version: use plain string package name to make it work with pyinstaller
    See-Also: https://github.com/pyinstaller/pyinstaller/pull/5830

--- apt_mirror/version.py
+++ apt_mirror/version.py
@@ -3,6 +3,8 @@
 from importlib.metadata import PackageNotFoundError, version
 
 try:
-    __version__ = version(__package__)
+    # We use plain string here instead of `__package__` to make pyinstaller
+    # distribution collection works.
+    __version__ = version("apt_mirror")
 except PackageNotFoundError:
     __version__ = "unknown"

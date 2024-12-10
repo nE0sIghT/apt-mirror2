04389912b1c7319906f08d5844fc8fce9aee55a7
Yuri Konotopov <ykonotopov@gnome.org>
2024-12-10 17:37:54 +0000

    requirements: bump caio to 0.9.17
    caio < 0.9.14 doesn't support Python 3.12
    
    Fixes: https://gitlab.com/apt-mirror2/apt-mirror2/-/issues/25

--- regenerate-dependencies.sh
+++ regenerate-dependencies.sh
@@ -2,8 +2,8 @@
 
 set -exo pipefail
 
-pip-compile --generate-hashes --output-file=requirements/prod.txt
-pip-compile --constraint=requirements/prod.txt --extra=uvloop --generate-hashes --output-file=requirements/uvloop.txt
-pip-compile --constraint=requirements/prod.txt --extra=dev --generate-hashes --allow-unsafe --output-file=requirements/dev.txt
-pip-compile --constraint=requirements/prod.txt --extra=aiofiles --generate-hashes --output-file=requirements/aiofiles.txt
-pip-compile --constraint=requirements/prod.txt --extra=prometheus --generate-hashes --output-file=requirements/prometheus.txt
+pip-compile --generate-hashes --output-file=requirements/prod.txt "${@}"
+pip-compile --constraint=requirements/prod.txt --extra=uvloop --generate-hashes --output-file=requirements/uvloop.txt "${@}"
+pip-compile --constraint=requirements/prod.txt --extra=dev --generate-hashes --allow-unsafe --output-file=requirements/dev.txt "${@}"
+pip-compile --constraint=requirements/prod.txt --extra=aiofiles --generate-hashes --output-file=requirements/aiofiles.txt "${@}"
+pip-compile --constraint=requirements/prod.txt --extra=prometheus --generate-hashes --output-file=requirements/prometheus.txt "${@}"
